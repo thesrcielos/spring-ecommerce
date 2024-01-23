a90f031727bcbb4198537b74c8dc63142de6483c
thesrcielos <diegomadi75@gmail.com>
2024-01-23 08:42:14 -0500

    creacion metodo post para guardar producto

--- demo/src/main/java/com/damd/demo/controller/ProductosController.java
+++ demo/src/main/java/com/damd/demo/controller/ProductosController.java
@@ -1,12 +1,18 @@
 package com.damd.demo.controller;
 
+import com.damd.demo.model.Producto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductosController {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductosController.class);
     @GetMapping("")
     public String show(){
         return "productos/show";
@@ -15,5 +21,11 @@ public class ProductosController {
     @GetMapping("/create")
     public String create(){
         return "productos/create";
+    }
+
+    @PostMapping("/save")
+    public String save(Producto producto){
+        LOGGER.info("Este es el objeto producto {}",producto);
+        return "redirect:/productos";
     }
 }
