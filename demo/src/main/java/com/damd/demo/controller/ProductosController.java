619effe25bd65c0e9ddcd72241cbee1ba8cf14cc
thesrcielos <diegomadi75@gmail.com>
2024-01-23 14:15:10 -0500

    funcionalidad de editar y eliminar productos

--- demo/src/main/java/com/damd/demo/controller/ProductosController.java
+++ demo/src/main/java/com/damd/demo/controller/ProductosController.java
@@ -3,12 +3,16 @@ package com.damd.demo.controller;
 import com.damd.demo.model.Producto;
 import com.damd.demo.model.Usuario;
 import com.damd.demo.service.ProductoService;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -38,6 +42,27 @@ public class ProductosController {
         Usuario usuario = new Usuario(1,"","","","","","","");
         producto.setUsuario(usuario);
         productoService.save(producto);
+        return "redirect:/productos";
+    }
+    
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model){
+        Optional<Producto> optionalProducto = productoService.get(id);
+        Producto producto = optionalProducto.get();
+        LOGGER.info("producto buscado {}",producto);
+        model.addAttribute("producto", producto);
+        return "productos/edit";
+    }
+    
+    @PutMapping("/update")
+    public String update(Producto producto){
+        productoService.update(producto);
+        return "redirect:/productos";
+    }
+    
+    @DeleteMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id){
+        productoService.delete(id);
         return "redirect:/productos";
     }
 }
